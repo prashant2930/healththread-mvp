d6a7e4e10918971852e84d6a825aa86d07d69c1b
Prashant Srivastava <prashant.sri2930@gmail.com>
2026-07-16 14:58:48 +0530

    Updates

---
+++ assets/ProfileDetailPage-ubCwU7Ea.js
@@ -0,0 +1,1 @@
+import{t as e}from"./jsx-runtime-DUAcabCT.js";var t=e();function n(){return(0,t.jsx)(`div`,{className:`p-8`,children:(0,t.jsx)(`h1`,{className:`text-2xl font-bold`,children:`ProfileDetailPage`})})}export{n as ProfileDetailPage};
